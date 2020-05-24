33c2bf57dc409a48fdaeac5e2dfc8024d7391fbe
amrabuaza <amr@insalat.com>
2020-05-24 19:49:01 +0300

    add translte messages

--- advanced/frontend/messages/ar/app.php
+++ advanced/frontend/messages/ar/app.php
@@ -1,1 +1,61 @@
 <?php
+$siteViews = [
+    'site.view.login' => 'تسجيل الدخول',
+    'site.view.remember_me' => ' تذكرني',
+    'site.view.username' => 'اسم المستخدم',
+    'site.view.password' => 'كلمة المرور',
+    'site.view.create_an_account' => 'إنشاء حساب؟',
+    'site.view.sign_up' => 'تسجيل حساب جديد',
+    'site.view.my_shops' => 'متاجري',
+    'site.view.profile' => 'الملف الشخصي',
+    "site.view.login_form_error" => "اسم المستخدم أو كلمة المرور غير صحيحة.",
+    "nav.logout" => "تسجيل الخرج",
+
+    "site.index.search" => "بحث",
+    "site.index.items" => "العناصر",
+
+    "site.login.welcome" => "أهلا بك",
+    'site.sign_up.email' => 'البريد الإلكتروني',
+];
+
+$shop = [
+    "shop.fields.name" => "الاسم",
+    "shop.fields.name_ar" => "الاسم بالعربية",
+    "shop.fields.phone_number" => "رقم الهاتف",
+    "shop.fields.description" => "الوصف",
+    "shop.fields.description_ar" => "الوصف باللغة العربية",
+    "shop.fields.open_at" => "فتح عند",
+    "shop.fields.close_at" => "إغلاق عند",
+    "shop.fields.rate" => "التقيم",
+    "shop.fields.status" => "الحالة",
+    "shop.fields.picture" => "صورة",
+    "shop.fields.pick_image" => "اختر صورة",
+    "shop.add_shop" => "إضافة متجر",
+    "shop.update_shop" => "تحديث متجر",
+];
+
+$category = [
+    'category.title' => "التصنيفات",
+    'category.add_btn' => "إضافة فئة",
+    'category.update_btn' => "تحديث الفئة",
+    'category.add_name_btn' => "أضف اسم الفئة",
+    'category.fields.name' => "الاسم",
+    'category.fields.name_ar' => "الاسم بالعربية",
+    'category.added_message_1' => "قبل المسؤول اسم الفئة",
+    'category.added_message_2' => "قبل المسؤول أسماء الفئات",
+    'category.add_message' => "شكرا لك على إضافة اسم الفئة. سنقبل به في أقرب وقت ممكن.",
+];
+
+$buttons = [
+    "buttons.update" => "تحديث",
+    "buttons.feedback" => "موجز",
+    "buttons.send" => "إرسال",
+    "buttons.delete" => "حذف",
+    "buttons.submit" => "التأكد",
+    "buttons.save" => "حفظ",
+    "buttons.add" => "إضافة",
+    "buttons.filter" => "تصفية",
+    "buttons.edit_password" => "تحرير كلمة المرور !!",
+];
+
+return array_merge($siteViews, $shop, $buttons, $category);
--- advanced/frontend/messages/en-US/app.php
+++ advanced/frontend/messages/en-US/app.php
@@ -1,1 +1,62 @@
 <?php
+$siteViews = [
+    'site.view.login' => 'Login',
+    'site.view.remember_me' => 'Remember Me',
+    'site.view.username' => 'Username',
+    'site.view.password' => 'Password',
+    'site.view.create_an_account' => 'Create an account?',
+    'site.view.sign_up' => 'Sign up',
+    'site.view.my_shops' => 'My Shops',
+    'site.view.profile' => 'Profile',
+    "site.view.login_form_error" => "Incorrect username or password.",
+    "nav.logout" => "Logout",
+
+    "site.index.search" => "Search",
+    "site.index.items" => "Items",
+
+    "site.login.welcome" => "WELCOME",
+    'site.sign_up.email' => 'Email',
+
+];
+
+$shop = [
+    "shop.fields.name" => "Name",
+    "shop.fields.name_ar" => "Name in Arabic",
+    "shop.fields.phone_number" => "Phone Number",
+    "shop.fields.description" => "Description",
+    "shop.fields.description_ar" => "Description in Arabic",
+    "shop.fields.open_at" => "Open At",
+    "shop.fields.close_at" => "Close At",
+    "shop.fields.rate" => "Rate",
+    "shop.fields.status" => "Status",
+    "shop.fields.picture" => "Picture",
+    "shop.fields.pick_image" => "Pick image",
+    "shop.add_shop" => "Add Shop",
+    "shop.update_shop" => "Update Shop",
+];
+
+$category = [
+    'category.title' => "Categories",
+    'category.add_btn' => "Add Category",
+    'category.update_btn' => "Update Category",
+    'category.add_name_btn' => "Add Category Name",
+    'category.fields.name' => "Name",
+    'category.fields.name_ar' => "Name in Arabic",
+    'category.added_message_1' => "Admin accepted the category name",
+    'category.added_message_2' => "Admin accepted the category names",
+    'category.add_message' => "Thank you for adding category name. We will accepted it as soon as possible.",
+];
+
+$buttons = [
+    "buttons.update" => "Update",
+    "buttons.feedback" => "Feedback",
+    "buttons.send" => "Send",
+    "buttons.delete" => "Delete",
+    "buttons.submit" => "Submit",
+    "buttons.save" => "Save",
+    "buttons.add" => "Add",
+    "buttons.filter" => "Filter",
+    "buttons.edit_password" => "Edit Password !!",
+];
+
+return array_merge($siteViews, $shop, $buttons, $category);
